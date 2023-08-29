0f35152e822d9996013f2c2cc41026a4c690e512
Andrei Murillo <murillosca@gmail.com>
2023-08-29 08:04:15 -0600

    Inicializacion del proyecto, instalacion de dependencias, librerias y paquetes

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+using API.data;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = _context.Users.ToList();
+        return users;
+    }
+}
