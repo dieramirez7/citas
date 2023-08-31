9d2d94040e6cb2d87013435335f7bce8f4d8132c
Andrei Murillo <murillosca@gmail.com>
2023-08-31 07:41:41 -0600

    Added Angular to the project

---
+++ API/Controllers/citasBD.sql
@@ -0,0 +1,10 @@
+
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"Juan Salazar");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"Jose Botella");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"Juan Pelao");
