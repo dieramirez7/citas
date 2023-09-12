89257ac89c5605b195c480808b2bbaef93e5b5e7
Andrei Murillo <murillosca@gmail.com>
2023-09-11 18:29:33 -0600

    Added Base Api Controller and made api params global, added password fields in db

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,5 +5,9 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; } // this is the email
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
     }
 }
