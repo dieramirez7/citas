7360f2a7474d68b8fe6d106f374eb6e721976378
Andrei Murillo <murillosca@gmail.com>
2023-09-25 07:25:02 -0600

    Added and linked JWT to ID of user for auth

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,9 +1,12 @@
 using API.data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
+
+[Authorize]
 public class UsersController : BaseApiController
 {
 
@@ -12,12 +15,16 @@ public class UsersController : BaseApiController
     {
         _context = context;
     }
+
+    [AllowAnonymous]
     [HttpGet]
 
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
         return await _context.Users.ToListAsync();
     }
+
+    [Authorize]
     [HttpGet("{id}")]
 
     public async Task<ActionResult<AppUser>> GetUser(int id)
