8369050035be8dde971717d02f6d16ccad3fc09e
Andrei Murillo <murillosca@gmail.com>
2023-10-12 07:41:31 -0600

    Modified frontend with bootswatch library and added auth guard

---
+++ SPA/src/app/_guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { CanActivateFn } from '@angular/router';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { inject } from '@angular/core';
+import { map } from 'rxjs';
+
+export const authGuard: CanActivateFn = (route, state) => {
+const accountService = inject(AccountService);
+const toastr = inject(ToastrService);
+
+
+  return accountService.currentUser$.pipe(
+    map( user => {
+      if (user) return true;
+      else {
+        toastr.error("Debes ingresar al sistema")
+        return false;
+      }
+    }
+    )
+  );
+};
--- SPA/src/app/nav/nav.component.ts
+++ SPA/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { IUser } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,8 +14,9 @@ export class NavComponent implements OnInit{
   model: any = {};
   // currentUser$: Observable<IUser | null> = of(null);
   // loggedIn = false;
+  
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     // this.currentUser$ = this.accountService.currentUser$;
@@ -23,14 +26,13 @@ export class NavComponent implements OnInit{
 
   login():void {
     this.accountService.login(this.model).subscribe({ 
-      next: response => {
-      console.log(response);
-    }, 
-    error: error => console.log(error)
+      next:_ => this.router.navigateByUrl("/members"),
+      error: error => this.toastr.error(error.error)
     })
   }
 
   logout(): void {
     this.accountService.logout();
+    this.router.navigateByUrl("/");
   }
 }
