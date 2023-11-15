cf74ea4eddf470a86a85284bf663a9b9aa677bae
Andrei Murillo <murillosca@gmail.com>
2023-11-15 07:44:54 -0600

    Seccion 10 Added Member card and photo gallery

--- SPA/src/app/_modules/shared.module.ts
+++ SPA/src/app/_modules/shared.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
-
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    TabsModule.forRoot(),
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
       timeOut: 10000,
@@ -19,7 +20,8 @@ import { ToastrModule } from 'ngx-toastr';
   ],
   exports: [
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule
   ]
 })
 export class SharedModule { }
