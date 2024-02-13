30c0f5b55e72dec62de3995a3800b0a3272b4c29
Diego Ramírez <diego42531@gmail.com>
2024-02-13 07:56:00 +0100

    Added and implemented member edit page

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.data;
 using API.DTOs;
 using API.Entities;
@@ -34,6 +35,18 @@ public class UsersController : BaseApiController
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
         return await _userRepository.GetMemberAsync(username);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+        if (user == null) return NotFound();
+        _mapper.Map(memberUpdateDto, user);
+        _userRepository.Update(user);
+        if (await _userRepository.SaveAllAsync()) return NoContent();
+        return BadRequest("Failed to update user");
     }
 
 }
